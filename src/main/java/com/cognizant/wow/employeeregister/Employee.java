a6f7c7a23542dee88b689843e203db49770fdd66
Prateek Mulye <prateekmulye@hotmail.com>
2018-12-17 17:05:48 -0500

    removed unused imports

--- src/main/java/com/cognizant/wow/employeeregister/Employee.java
+++ src/main/java/com/cognizant/wow/employeeregister/Employee.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
--- src/test/java/com/cognizant/wow/employeeregister/EmployeeRegisterationControllerTest.java
+++ src/test/java/com/cognizant/wow/employeeregister/EmployeeRegisterationControllerTest.java
@@ -1,6 +1,5 @@
 package com.cognizant.wow.employeeregister;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
